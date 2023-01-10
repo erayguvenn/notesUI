63fc3fb91ed36fc1d0c4d9508fcd7717d84f4aa2
Eray Güven <eray0736@gmail.com>
2023-01-10 23:40:58 +0300

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,30 @@
 import { useEffect, useState } from "react";
 import Header from "../components/Header";
-import { GetAllNotes } from "../services/note";
+import { CreateNote, DeleteNote, GetAllNotes } from "../services/note";
 import useAuthStore from "../store/auth";
 import { motion, AnimatePresence } from "framer-motion";
-
-import { RiSearchLine } from "react-icons/ri";
+import { v4 as uuidv4 } from "uuid";
+import {
+  RiSearchLine,
+  RiDeleteBin5Line,
+  RiAddCircleFill,
+} from "react-icons/ri";
+import produce from "immer";
 
 function HomePage() {
   const token = useAuthStore((state) => state.token);
   const [notes, setNotes] = useState([]);
   const [selectedNote, setSelectedNote] = useState(null);
+  const [isCreatingNote, setIsCreatingNote] = useState(false);
 
+  const [createNoteBody, setCreateNoteBody] = useState("");
+  const [createNoteTitle, setCreateNoteTitle] = useState("");
   useEffect(() => {
     async function getNotes() {
       try {
         const notes = await GetAllNotes(token);
         notes[0].body =
-          "AHAH AHA HAH AH A A AHA AH AHAHAHAHA HA AH AH AH A AHAH AHA HAH AH A A AHA AH AHAHAHAHA HA AH AH AH A AHAH AHA HAH AH A A AHA AH AHAHAHAHA HA AH AH AH A";
+          "AHAH  HAH AH A A AHA AH AHAHAHAHA HA AH AH AH A AHAH AHA HAH AH A A AHA AH AHAHAHAHA HA AH AH AH A AHAH AHA HAH AH A A AHA AH AHAHAHAHA HA AH AH AH A";
         setNotes([...notes]);
       } catch (err) {
         console.error(err);
@@ -27,7 +35,59 @@ function HomePage() {
   }, []);
 
   const dummy = [{}, {}, {}, {}];
-  console.log(notes);
+
+  const onClickDelete = async (e, noteId) => {
+    e.stopPropagation();
+
+    setNotes(notes.filter((note) => note.id !== noteId));
+    try {
+      await DeleteNote(token, noteId);
+    } catch (err) {}
+  };
+
+  const onCreateNote = () => {
+    let title = createNoteTitle;
+    const body = createNoteBody;
+
+    if (title.trim() == "") title = "Başlıksız";
+
+    console.log("Şu bilgilerle not oluşturuluyor", title, body);
+
+    let temporaryId = uuidv4();
+
+    setNotes((notes) => [
+      {
+        temporaryId,
+        title,
+        body,
+        isCreating: true,
+      },
+      ...notes,
+    ]);
+
+    CreateNote(token, title, body).then((newNote) =>
+      setNotes(
+        produce((oldNotes) => {
+          let noteToUpdate = oldNotes.find(
+            (note) => note.temporaryId == temporaryId
+          );
+
+          if (noteToUpdate) {
+            Object.entries(newNote).forEach(
+              ([key, value]) => (noteToUpdate[key] = value)
+            );
+            noteToUpdate.isCreating = false;
+            console.log(noteToUpdate);
+          }
+        })
+      )
+    );
+    setCreateNoteTitle("");
+    setCreateNoteBody("");
+
+    setIsCreatingNote(false);
+  };
+
   return (
     <div className="w-full flex-1 max-w-5xl py-12 px-4 mx-auto flex flex-col">
       <Header />
@@ -45,18 +105,58 @@ function HomePage() {
           <h3>Notlar</h3>
           <div className="h-full flex flex-col overflow-auto scrollbar-none pb-32">
             <div className="grid grid-cols-3 gap-8 auto-rows-[9rem] mt-4 flex-1">
-              {notes.map((note, i) => (
-                <motion.div
-                  key={note.id}
-                  layoutId={note.id}
-                  className={`bg-darkBlack p-4 rounded-lg overflow-hidden cursor-pointer ${
-                    i == 1 && "row-span-2"
-                  }`}
-                  onClick={(e) => setSelectedNote(note)}
-                >
-                  <p>{note.body}</p>
-                </motion.div>
-              ))}
+              <AnimatePresence mode="popLayout">
+                {!isCreatingNote && (
+                  <motion.div
+                    animate={{
+                      opacity: 0.25,
+                    }}
+                    whileHover={{
+                      opacity: 0.5,
+                    }}
+                    onClick={() => setIsCreatingNote(true)}
+                    layoutId="createNote"
+                    className="flex items-center justify-center bg-darkBlack/25 rounded-lg cursor-pointer"
+                  >
+                    <RiAddCircleFill className="w-6 h-6" />
+                  </motion.div>
+                )}
+                {isCreatingNote && <div></div>}
+                {notes.map((note, i) => (
+                  <motion.div
+                    key={note.temporaryId || note.id}
+                    layoutId={note.temporaryId || note.id}
+                    className={`bg-darkBlack p-4 rounded-lg overflow-hidden cursor-pointer relative group
+                    ${note.isCreating && "animate-pulse"}`}
+                    exit={{
+                      scale: 0.1,
+                    }}
+                    onClick={(e) =>
+                      note.isCreating ? null : setSelectedNote(note)
+                    }
+                  >
+                    <div>
+                      <div className="title flex justify-between items-center">
+                        <div className="relative flex-1 overflow-hidden">
+                          <p className="whitespace-nowrap overflow-ellipsis overflow-hidden">
+                            {note.title}
+                          </p>
+                        </div>
+                        <button
+                          onClick={(e) =>
+                            note.isCreating ? null : onClickDelete(e, note.id)
+                          }
+                          className="mx-2 shrink-0"
+                        >
+                          <RiDeleteBin5Line className="w-4" />
+                        </button>
+                      </div>
+                      <p className="text-white/75 text-sm mt-2 ">{note.body}</p>
+                      <p className="text-green-500">{note.date}</p>
+                    </div>
+                  </motion.div>
+                ))}
+              </AnimatePresence>
             </div>
           </div>
         </div>
@@ -72,12 +172,60 @@ function HomePage() {
             onClick={(e) => setSelectedNote(null)}
           >
             <motion.div
-              layoutId={selectedNote.id}
+              layoutId={selectedNote.temporaryId || selectedNote.id}
               className={`bg-darkBlack p-4 rounded-lg overflow-hidden cursor-pointer w-96 h-96`}
               onClick={(e) => e.stopPropagation()}
             >
-              <p>{selectedNote.body}</p>
+              <div className="title flex justify-between items-center">
+                <div className="relative flex-1 overflow-hidden">
+                  <p className="whitespace-nowrap overflow-ellipsis overflow-hidden">
+                    {selectedNote.title}
+                  </p>
+                </div>
+                <button
+                  onClick={(e) => onClickDelete(e, selectedNote.id)}
+                  className="mx-2 shrink-0"
+                >
+                  <RiDeleteBin5Line className="w-4" />
+                </button>
+              </div>
+              <p className="text-white/75 text-sm mt-2 ">{selectedNote.body}</p>
             </motion.div>
+          </motion.div>
+        )}
+
+        {isCreatingNote && (
+          <motion.div
+            initial={{ backgroundColor: "#00000000" }}
+            animate={{ backgroundColor: "#000000aa" }}
+            exit={{ backgroundColor: "#00000000" }}
+            className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center"
+            onClick={(e) => (onCreateNote(), setIsCreatingNote(false))}
+          >
+            <form onSubmit={(e) => (e.preventDefault(), onCreateNote())}>
+              <motion.div
+                layoutId="createNote"
+                className={`bg-darkBlack p-4 rounded-lg overflow-hidden cursor-pointer w-96 h-96 flex flex-col`}
+                onClick={(e) => e.stopPropagation()}
+              >
+                <div className="flex">
+                  <input
+                    type="text"
+                    placeholder="Başlık.."
+                    value={createNoteTitle}
+                    onChange={(e) => setCreateNoteTitle(e.target.value)}
+                    className="bg-transparent outline-none flex-1 focus:bg-white/10 p-2 transition rounded"
+                  />
+                </div>
+                <textarea
+                  type="text"
+                  value={createNoteBody}
+                  onChange={(e) => setCreateNoteBody(e.target.value)}
+                  placeholder="İçerik..."
+                  className="bg-transparent outline-none flex-1 mt-2 p-2 rounded focus:bg-white/10 transition text-start"
+                />
+              </motion.div>
+            </form>
           </motion.div>
         )}
       </AnimatePresence>
--- src/services/note.js
+++ src/services/note.js
@@ -12,3 +12,38 @@ export async function GetAllNotes(token) {
 
   return json;
 }
+
+export async function CreateNote(token, title, body){
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/note`, {
+    method: "POST",
+    headers: {
+      Authorization: `${token}`,
+      "Content-Type": "application/json",
+    },
+    body:JSON.stringify({
+      title,
+      body
+    })
+  });
+
+  const json = await response.json();
+
+  if (!response.ok) throw new Error(json.message);
+
+  return json;
+}
+
+export async function DeleteNote(token, id){
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/note/${id}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `${token}`,
+    },
+  });
+
+  const json = await response.json();
+
+  if (!response.ok) throw new Error(json.message);
+
+  return json;
+}
